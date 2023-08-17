93513532a917dc150fabca65250710ddb716c20b
tenporpuh <117576048+tenporpuh@users.noreply.github.com>
2023-08-17 13:33:28 +0100

    Add files via upload

---
+++ JavascriptObjects.js
@@ -0,0 +1,14 @@
+//declearing and using objects in js
+const apple = {
+    color: 'Green',
+    price: {
+    bulk: '3000/kg',
+    smallQty: '4000/kg'
+    }
+   };
+   console.log(apple.color);
+   
+    // print the word 'Green' 
+   console.log(apple.price.bulk); 
+   // print the price ‘3000/kg
+   
