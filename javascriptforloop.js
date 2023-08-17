67f90b991493a65f7ad82651e46b60e36395743d
tenporpuh <117576048+tenporpuh@users.noreply.github.com>
2023-08-17 13:34:42 +0100

    Add files via upload

---
+++ javascriptforloop.js
@@ -0,0 +1,10 @@
+// declearing for ...in loop in js
+let mobile = {
+    brand: 'Samsung',
+    model: 'Galaxy Note 22',
+    price:'25ok',
+   };
+   for (let key in mobile) {
+    console.log(`${key}: ${mobile[key]}`);
+   }
+   
